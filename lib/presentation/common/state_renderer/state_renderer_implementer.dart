6c8acd53ff7dba399a206509365edf445031477a
Ahmad jz <ahmadjzz99@gmail.com>
2023-03-18 22:13:07 +0300

    create state renderer
    create state renderer for handling the state of our app manually with streams using RxDart and MVVM pattern

---
+++ lib/presentation/common/state_renderer/state_renderer_implementer.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app_mvvm/app/resources/constants_manager.dart';
+import 'package:movies_app_mvvm/app/resources/strings_manager.dart';
+import 'package:movies_app_mvvm/presentation/common/state_renderer/state_renderer.dart';
+
+abstract class FlowState {
+  StateRendererType getStateRendererType();
+
+  String getMessage();
+}
+
+class LoadingState extends FlowState {
+  StateRendererType stateRendererType;
+  String? message;
+
+  LoadingState(
+      {required this.stateRendererType, String message = AppStrings.loading});
+
+  @override
+  String getMessage() => message ?? AppStrings.loading;
+
+  @override
+  StateRendererType getStateRendererType() => stateRendererType;
+}
+
+class ErrorState extends FlowState {
+  StateRendererType stateRendererType;
+  String message;
+
+  ErrorState(this.stateRendererType, this.message);
+
+  @override
+  String getMessage() => message;
+
+  @override
+  StateRendererType getStateRendererType() => stateRendererType;
+}
+
+class ContentState extends FlowState {
+  ContentState();
+
+  @override
+  String getMessage() => AppConstants.empty;
+
+  @override
+  StateRendererType getStateRendererType() => StateRendererType.contentState;
+}
+
+class EmptyState extends FlowState {
+  String message;
+
+  EmptyState(this.message);
+
+  @override
+  String getMessage() => message;
+
+  @override
+  StateRendererType getStateRendererType() =>
+      StateRendererType.fullScreenEmptyState;
+}
+
+class SuccessState extends FlowState {
+  String message;
+
+  SuccessState(this.message);
+
+  @override
+  String getMessage() => message;
+
+  @override
+  StateRendererType getStateRendererType() => StateRendererType.popupSuccess;
+}
+
+extension FlowStateExtension on FlowState {
+  Widget getScreenWidget({
+    required BuildContext context,
+    required Widget contentScreenWidget,
+    required Function retryActionFunction,
+  }) {
+    switch (runtimeType) {
+      case LoadingState:
+        {
+          if (getStateRendererType() == StateRendererType.popupLoadingState) {
+            showPopup(context, getStateRendererType(), getMessage());
+            return contentScreenWidget;
+          } else {
+            return StateRenderer(
+                message: getMessage(),
+                stateRendererType: getStateRendererType(),
+                retryActionFunction: retryActionFunction);
+          }
+        }
+      case ErrorState:
+        {
+          dismissDialog(context);
+          if (getStateRendererType() == StateRendererType.popupErrorState) {
+            showPopup(context, getStateRendererType(), getMessage());
+            return contentScreenWidget;
+          } else {
+            return StateRenderer(
+                message: getMessage(),
+                stateRendererType: getStateRendererType(),
+                retryActionFunction: retryActionFunction);
+          }
+        }
+      case EmptyState:
+        {
+          return StateRenderer(
+              stateRendererType: getStateRendererType(),
+              message: getMessage(),
+              retryActionFunction: () {});
+        }
+      case ContentState:
+        {
+          dismissDialog(context);
+          return contentScreenWidget;
+        }
+      case SuccessState:
+        {
+          dismissDialog(context);
+          showPopup(context, StateRendererType.popupSuccess, getMessage(),
+              title: AppStrings.success);
+          return contentScreenWidget;
+        }
+      default:
+        {
+          dismissDialog(context);
+          return contentScreenWidget;
+        }
+    }
+  }
+
+  _isCurrentDialogShowing(BuildContext context) =>
+      ModalRoute.of(context)?.isCurrent != true;
+
+  dismissDialog(BuildContext context) {
+    if (_isCurrentDialogShowing(context)) {
+      Navigator.of(context, rootNavigator: true).pop(true);
+    }
+  }
+
+  showPopup(
+      BuildContext context, StateRendererType stateRendererType, String message,
+      {String title = AppConstants.empty}) {
+    WidgetsBinding.instance.addPostFrameCallback((_) => showDialog(
+        context: context,
+        builder: (BuildContext context) => StateRenderer(
+            stateRendererType: stateRendererType,
+            message: message,
+            title: title,
+            retryActionFunction: () {})));
+  }
+}
